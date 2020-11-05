71f57da0e61db99b11d1ca1f329a3929085c88bd
Konstantinchik <kgluschenko83@gmail.com>
2020-11-05 21:33:23 +0300

    Сохраним Task3

--- TestLesson/Program.cs
+++ TestLesson/Program.cs
@@ -12,9 +12,17 @@ namespace TestLesson
         /// <summary>
         /// Найти два наибольших элемента массива
         /// </summary>
-        static void Task2()
+        public static void Task2()
         {
             int[] a = new int[] { 5, 12, 13, 14 };
+        }
+
+        /// <summary>
+        /// Новая веточка
+        /// </summary>
+        public static void Task3()
+        {
+            int[] a = new int[] { 6, 7, 9, 9, 0 };
         }
     }
 }
