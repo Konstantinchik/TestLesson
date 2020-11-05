15ab807d242fe7aae012cc2606f376e250b453ab
Konstantinchik <kgluschenko83@gmail.com>
2020-11-05 20:35:09 +0300

    Добавьте файлы проекта.

---
+++ TestLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
