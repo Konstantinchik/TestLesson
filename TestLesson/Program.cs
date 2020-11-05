64ae2237f85122c9d065d539c5ea98efd5644343
Konstantinchik <kgluschenko83@gmail.com>
2020-11-05 21:12:11 +0300

    исправил Task2

--- TestLesson/Program.cs
+++ TestLesson/Program.cs
@@ -8,5 +8,13 @@ namespace TestLesson
         {
             Console.WriteLine("Hello World!");
         }
+
+        /// <summary>
+        /// Найти два наибольших элемента массива
+        /// </summary>
+        static void Task2()
+        {
+            int[] a = new int[] { 5, 12, 13, 14 };
+        }
     }
 }
