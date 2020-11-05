ddeda78400d5faed40c3e81771ef4ebcf732ed8a
Konstantinchik <kgluschenko83@gmail.com>
2020-11-05 21:52:29 +0300

    добавил новое задание упр 13

--- TestLesson/Program.cs
+++ TestLesson/Program.cs
@@ -24,5 +24,13 @@ namespace TestLesson
         {
             int[] a = new int[] { 6, 7, 9, 9, 0 };
         }
+
+        /// <summary>
+        /// Одномерный массив скопировать в двумерный 3 yf 3
+        /// </summary>
+        public static void Task13()
+        {
+            int[] m = new int[] { 3, 5, 1, 9, 0, 5, 8, 0 };
+        }
     }
 }
